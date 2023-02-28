4517aec2b076ef99c0b93cd725755c9e50b5e4a1
Shoxruz <shoxa12abdu@gmail.com>
2023-02-28 19:50:16 +0500

    I'm completed AddressController

---
+++ src/main/java/com/example/student_management/controller/AddressController.java
@@ -0,0 +1,55 @@
+package com.example.student_management.controller;
+
+import com.example.student_management.entity.Address;
+import com.example.student_management.entity.Faculty;
+import com.example.student_management.repository.AddressRepo;
+import com.example.student_management.repository.FacultyRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class AddressController {
+
+    @Autowired
+    private AddressRepo addressRepo;
+
+
+    @GetMapping("address")
+    public List<Address> getAll() {
+        return addressRepo.findAll();
+    }
+
+
+    @PostMapping("address")
+    public String add(@RequestBody Address address) {
+        addressRepo.save(address);
+        return "success";
+    }
+
+
+    @DeleteMapping("address/{id}")
+    public String delete(@PathVariable Long id) {
+        addressRepo.deleteById(id);
+        return "success";
+    }
+
+    @PutMapping("editt/{id}")
+    public String update(@RequestBody Address address, @PathVariable Long id) {
+        Optional<Address> byId = addressRepo.findById(id);
+        if (byId.isPresent()) {
+            Address editedAddress = byId.get();
+            editedAddress.setId(address.getId());
+            editedAddress.setCountry(address.getCountry());
+            editedAddress.setStreet(address.getStreet());
+            editedAddress.setHomeNumber(address.getHomeNumber());
+            editedAddress.setRegion(address.getRegion());
+            addressRepo.save(editedAddress);
+            return "success";
+        } else return "faculty by this id is not found";
+    }
+
+
+}
